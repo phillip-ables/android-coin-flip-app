7c5fc9c1cc0e405f31cc53efd828e024037e4fcd
Phillip <phillipables1@gmail.com>
2018-02-14 08:43:37 -0500

    set up new Random and coinside integer

--- app/src/main/java/com/example/techtron/coinflipapp/MainActivity.java
+++ app/src/main/java/com/example/techtron/coinflipapp/MainActivity.java
@@ -6,10 +6,14 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 
+import java.util.Random;
+
 public class MainActivity extends AppCompatActivity {
 
     Button b_flip;
     ImageView iv_coin;
+    Random r;
+    int coinSide; //0-head 1-tailsd
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,10 +22,12 @@ public class MainActivity extends AppCompatActivity {
 
         b_flip = (Button) findViewById(R.id.btn_flip);
         iv_coin = (ImageView) findViewById(R.id.iv_coin);
+        r = new Random();
 
         b_flip.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                coinSide = r.nextInt(2);
 
             }
         });
