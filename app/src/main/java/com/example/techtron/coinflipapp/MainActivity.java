2bc72e0b84dd1e447fef0eaffd70894cf67e38a4
Phillip <phillipables1@gmail.com>
2018-02-14 08:37:30 -0500

    set up button and image view in main activity

--- app/src/main/java/com/example/techtron/coinflipapp/MainActivity.java
+++ app/src/main/java/com/example/techtron/coinflipapp/MainActivity.java
@@ -2,12 +2,20 @@ package com.example.techtron.coinflipapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button b_flip;
+    ImageView iv_coin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        b_flip = (Button) findViewById(R.id.btn_flip);
+        iv_coin = (ImageView) findViewById(R.id.iv_coin);
     }
 }
